29e006a78dcc83b08c834ad8b749119507f11fea
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-12-01 13:37:04 +0800

    Enhance Day 01 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,48 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var most = 0
-        val calories = mutableListOf<Int>()
+    fun part1(input: List<String>): Int =
+        input.joinToString(separator = "\n")
+            .split("\n\n").maxOfOrNull {
+                it.split("\n")
+                    .map(String::toInt)
+                    .sum()
+            } ?: 0
 
-        for ((i, line) in input.withIndex()) {
-            if (line.isBlank() || i == input.lastIndex && calories.isNotEmpty()) {
-                val sum = calories.sum()
-                if (sum > most) {
-                    most = sum
-                }
-                calories.clear()
-            } else {
-                calories += line.toInt()
+    fun part2(input: List<String>): Int =
+        input.joinToString(separator = "\n")
+            .split("\n\n").map {
+                it.split("\n")
+                    .map(String::toInt)
+                    .sum()
             }
-        }
-
-        return most
-    }
-
-    fun part2(input: List<String>): Int {
-        val all = mutableListOf<Int>()
-        val calories = mutableListOf<Int>()
-
-        for ((i, line) in input.withIndex()) {
-            if (line.isBlank() || i == input.lastIndex && calories.isNotEmpty()) {
-                all += calories.sum()
-                calories.clear()
-            } else {
-                calories += line.toInt()
-            }
-        }
-
-        var sum = 0
-
-        for (i in 0 until 3) {
-            val currentMax = all.max()
-
-            all.remove(currentMax)
-
-            sum += currentMax
-        }
-
-        return sum
-    }
+            .sortedDescending()
+            .subList(0, 3)
+            .sum()
 
 //    val input = readInput("Day01_Example")
 //    println(part1(input))
