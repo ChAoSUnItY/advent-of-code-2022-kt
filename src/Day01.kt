e4ca388e6a5990cd86eda1ae4b8e5bc5c43f1bb9
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-12-01 13:24:09 +0800

    Complete Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var most = 0
+        val calories = mutableListOf<Int>()
+
+        for ((i, line) in input.withIndex()) {
+            if (line.isBlank() || i == input.lastIndex && calories.isNotEmpty()) {
+                val sum = calories.sum()
+                if (sum > most) {
+                    most = sum
+                }
+                calories.clear()
+            } else {
+                calories += line.toInt()
+            }
+        }
+
+        return most
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val all = mutableListOf<Int>()
+        val calories = mutableListOf<Int>()
+
+        for ((i, line) in input.withIndex()) {
+            if (line.isBlank() || i == input.lastIndex && calories.isNotEmpty()) {
+                all += calories.sum()
+                calories.clear()
+            } else {
+                calories += line.toInt()
+            }
+        }
+
+        var sum = 0
+
+        for (i in 0 until 3) {
+            val currentMax = all.max()
+
+            all.remove(currentMax)
+
+            sum += currentMax
+        }
+
+        return sum
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val input = readInput("Day01_Example")
+//    println(part1(input))
+//    println(part2(input))
 
     val input = readInput("Day01")
     println(part1(input))
