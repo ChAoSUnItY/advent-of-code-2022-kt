90d080b2ef52b24d55a5c9e768c446f1c4dd09ba
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-12-08 23:38:58 +0800

    Add solution for Day 08 (part 2)

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,22 +1,33 @@
+import kotlin.math.max
+
 typealias GridWalk = Pair<IntProgression, IntProgression>
+typealias GridCrossWalk = IntProgression
 
 fun main() {
     fun getRanges(size: Int): List<IntProgression> =
         listOf((0 until size), (size - 1 downTo 0))
 
-    fun directions(size: Int): List<GridWalk> =
+    fun getRanges(size: Int, start: Int): List<IntProgression> =
+        listOf((start - 1 downTo 0), (start + 1 until size))
+
+    fun gridWalk(size: Int): List<GridWalk> =
         getRanges(size).flatMap { y -> getRanges(size).map { x -> y to x } }
 
-    fun part1(data: List<String>): Int {
-        val heightMap = data.map {
+    fun gridCrossWalk(size: Int, startX: Int, startY: Int): List<GridCrossWalk> =
+        listOf(startX, startY).flatMap { getRanges(size, it) }
+    
+    fun processData(data: List<String>): List<List<Int>> =
+        data.map {
             it.toCharArray()
                 .filter(Char::isDigit)
                 .map(Char::digitToInt)
         }
+
+    fun part1(data: List<List<Int>>): Int {
         val size = data.size
         val visible = Array(size) { BooleanArray(size) }
 
-        for ((i, gridWalk) in directions(size).withIndex()) {
+        for ((i, gridWalk) in gridWalk(size).withIndex()) {
             val (xWalk, yWalk) = gridWalk
 
             for (y in yWalk) {
@@ -24,8 +35,8 @@ fun main() {
 
                 for (x in xWalk) {
                     val currentHeight =
-                        if (i % 2 == 0) heightMap[x][y]
-                        else heightMap[y][x]
+                        if (i % 2 == 0) data[x][y]
+                        else data[y][x]
 
                     if (currentHeight > currentMaxHeight) {
                         currentMaxHeight = currentHeight
@@ -44,6 +55,33 @@ fun main() {
             .count { it } + size * 4 - 4
     }
 
+    fun part2(data: List<List<Int>>): Int {
+        val size = data.size
+        var maxScenicScore = 0
+
+        for ((y, row) in data.withIndex()) {
+            for ((x, treeHouseHeight) in row.withIndex()) {
+                val directionScenicScores = IntArray(4)
+
+                for ((i, gridWalk) in gridCrossWalk(size, x, y).withIndex()) {
+                    for (offset in gridWalk) {
+                        directionScenicScores[i]++
+                        val height =
+                            if (i < 2) data[y][offset]
+                            else data[offset][x]
+                        if (height >= treeHouseHeight) break
+                    }
+                }
+
+                maxScenicScore = max(directionScenicScores.fold(1, Int::times), maxScenicScore)
+            }
+        }
+
+        return maxScenicScore
+    }
+
     val data = readInput("Day08")
-    println(part1(data))
+    val processedData = processData(data)
+    println(part1(processedData))
+    println(part2(processedData))
 }
