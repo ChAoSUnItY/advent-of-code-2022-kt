8be4902cc3b846ff2ae51b847a69205e31b1ba9a
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-12-09 17:07:22 +0800

    Minor refactor

--- src/Day09.kt
+++ src/Day09.kt
@@ -2,45 +2,48 @@ import kotlin.math.abs
 import kotlin.math.sign
 
 fun main() {
-    data class Position(val x: Int = 0, val y: Int = 0)
+    data class Position(val x: Int = 0, val y: Int = 0) {
+        fun offset(x: Int = 0, y: Int = 0): Position =
+            copy(x = this.x + x, y = this.y + y)
+    }
+
     data class Instruction(val direction: Char, val steps: Int)
-    
+
     fun processData(data: List<String>): List<Instruction> =
         data.map {
             val (direction, step) = it.split(" ")
             Instruction(direction[0], step.toInt())
         }
 
+    fun moveHead(head: Position, direction: Char): Position = when (direction) {
+        'U' -> head.offset(y = 1)
+        'D' -> head.offset(y = -1)
+        'R' -> head.offset(x = 1)
+        'L' -> head.offset(x = -1)
+        else -> head
+    }
+
     fun followHead(head: Position, tail: Position): Position = when {
-        head.x == tail.x && abs(head.y - tail.y) > 1 -> Position(tail.x, tail.y + (head.y - tail.y).sign)
-        head.y == tail.y && abs(head.x - tail.x) > 1 -> Position(tail.x + (head.x - tail.x).sign, tail.y)
-        abs(head.y - tail.y) + abs(head.x - tail.x) >= 3 -> Position(
-            tail.x + (head.x - tail.x).sign,
-            tail.y + (head.y - tail.y).sign
+        head.x == tail.x && abs(head.y - tail.y) > 1 -> tail.offset(y = (head.y - tail.y).sign)
+        head.y == tail.y && abs(head.x - tail.x) > 1 -> tail.offset(x = (head.x - tail.x).sign)
+        abs(head.y - tail.y) + abs(head.x - tail.x) >= 3 -> tail.offset(
+            x = (head.x - tail.x).sign,
+            y = (head.y - tail.y).sign
         )
+
         else -> tail
     }
 
-    fun traverse(data: List<Instruction>, children: Int): Int {
+    fun traverse(data: List<Instruction>, length: Int): Int {
         val traversedPositions = mutableSetOf<Position>()
-        var head = Position()
-        val rope = Array(children) { Position() }
+        val rope = Array(length) { Position() }
 
         for ((direction, step) in data) {
             for (_i in 0 until step) {
-                when (direction) {
-                    'U' -> head = Position(head.x, head.y + 1)
-                    'D' -> head = Position(head.x, head.y - 1)
-                    'R' -> head = Position(head.x + 1, head.y)
-                    'L' -> head = Position(head.x - 1, head.y)
-                }
+                rope[0] = moveHead(rope[0], direction)
 
-                for (i in rope.indices) {
-                    val first = if (i == 0) head else rope[i - 1]
-                    val tail = rope[i]
-                    val newPosition = followHead(first, tail)
-                    rope[i] = newPosition
-                }
+                for (i in 1 until rope.size) 
+                    rope[i] = followHead(rope[i - 1], rope[i])
 
                 traversedPositions += rope.last()
             }
@@ -50,10 +53,10 @@ fun main() {
     }
 
     fun part1(data: List<Instruction>): Int =
-        traverse(data, 1)
+        traverse(data, 2)
 
     fun part2(data: List<Instruction>): Int =
-        traverse(data, 9)
+        traverse(data, 10)
 
     val input = readInput("Day09")
     val data = processData(input)
